055d3b8f1569df84068925fcd805aaac8a6fdaed
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-03 16:41:55 +0530

    add:intial commite

---
+++ UC1.java
@@ -0,0 +1,16 @@
+package company;
+
+public class SnakeLadderGame {
+
+	public static void main(String[] args) {               
+		
+	
+		int player=1; //start the game with one player
+
+		//  player in position 0
+		int position=0;
+
+		System.out.println("Player in thosition = "+position);
+	}
+
+}
